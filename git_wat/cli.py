ebfc4fb90d747190585e26e8d0e9dd0aff187ab0
Ian Denhardt <ian@zenhack.net>
2015-11-22 19:55:43 -0500

    Start adding more cli structure
    Getting a token is now a sub-command.

--- git_wat/cli.py
+++ git_wat/cli.py
@@ -1,1 +1,18 @@
-from .github import prompt as main
+import argparse
+
+from . import github
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers()
+get_token_parser = subparsers.add_parser('get-token')
+
+
+def get_token(args):
+    github.prompt()
+
+get_token_parser.set_defaults(func=get_token)
+
+
+def main():
+    args = parser.parse_args()
+    args.func(args)
